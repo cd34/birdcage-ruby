850e38baab3b2d7fb595c0d6f5dd3f080945bf3a
Chris Davies <mcd@daviesinc.com>
2013-01-31 20:45:33 -0500

    include proper example

--- lib/example.rb
+++ lib/example.rb
@@ -1,23 +1,50 @@
 require "./birdcage"
 
-a = Text.new(['a', 'bb', 'ccc', 'dddd'])
-nums = Text.new([1, 22, 333, 4444])
-l = Text.new(['abcdefghijklmnopqrstuvwxyz'], {'trim'=>true})
-#l = Text.new(['abcdefghijklmnopqrstuvwxyz'])
-p = Phrase.new([l, nums])
-    
-#puts l.lens
-#l.select(0)
-#puts l.cur_len
-#l.select(1)
-#puts l.cur_len
-#l.select(3)
-#l.select_next
-#puts 'curtext'
-#puts l.cur_text
-#puts '' 
-#puts l.cur_len
-#puts l.next_len
+"""
+create a Text object with a series of possible strings
+"""
+a = Text.new(['one hour, fourty-five minutes', '1 hour, 45 minutes', '1h45m', 
+          '1:45'])
 
-puts p.non_trim_length
-puts p.generate({'length'=>20, 'delimiter'=>', '})
+"""
+create a Text object that has an additional phrase to be displayed
+after the first Text object. We will set the attribute trim which will allow
+this text to be sliced if there isn't enough room.
+"""
+b = Text.new(['until armageddon strikes the earth'], {'trim'=>true})
+
+"""
+Create the phrase from the Text objects
+"""
+phrase = Phrase.new([a, b])
+
+"""
+Generate the phrase
+"""
+puts 'No maximum set, generate phrase'
+puts 'expecting: one hour, fourty-five minutes until armageddon strikes the earth'
+puts 'received:  ' + phrase.generate
+puts 
+
+
+"""
+Select a different delimiter between the Text objects
+"""
+puts 'No maximum length set, generate phrase with ": " as the delimiter'
+puts 'expecting: one hour, fourty-five minutes: until armageddon strikes the earth'
+puts 'received:  ' + phrase.generate({'delimiter'=>': '})
+puts
+
+"""
+Set a maximum length for the message. Birdcage will try to compact the string
+gracefully.
+"""
+puts 'Set a maximum length of 40 characters'
+puts 'expecting: 1h45m until armageddon strikes the earth'
+puts 'received:  ' + phrase.generate({'length'=>40})
+puts
+
+puts 'Set a maximum length of 30 characters'
+puts 'expecting: 1h45m until armageddon stri...'
+puts 'received:  ' + phrase.generate({'length'=>30})
+puts
